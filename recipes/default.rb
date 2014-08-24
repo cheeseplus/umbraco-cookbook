2baececce26fd560f75907e25038d1ff4c4a3aad
Seth Thomas <sthomas@getchef.com>
2014-08-24 13:14:58 -0700

    Adding guard to prevent binding error

--- recipes/default.rb
+++ recipes/default.rb
@@ -65,8 +65,10 @@ iis_site 'umbraco' do
   action [:add,:start]
 end
 
+iis_config "-section:system.applicationHost/sites /-\"[name='umbraco'].bindings.[protocol='http',bindingInformation='*:#{node['umbraco']['port']}:']\" /commit:apphost"
 
 iis_config "-section:system.applicationHost/sites /+\"[name='umbraco'].bindings.[protocol='http',bindingInformation='*:#{node['umbraco']['port']}:']\" /commit:apphost" do
     action :config
     notifies :restart, "service[iis]"
+    not_if { node['umbraco']['port'] == 80 }
 end
